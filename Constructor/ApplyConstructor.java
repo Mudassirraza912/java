720d4cdf069afa21cdf56e458f6bb82e83270d45
Mudassir raza <razamudassir912@gmail.com>
2021-12-16 12:04:27 -0800

    first commit

---
+++ Constructor/ApplyConstructor.java
@@ -0,0 +1,6 @@
+// Use Constructor
+public class ApplyConstructor {
+    public static void main(String args[]) {
+        Constructor cl = new Constructor();
+    }
+}
---
+++ Constructor/Constructor.java
@@ -0,0 +1,6 @@
+// Create a Constructor class
+public class Constructor {
+    Constructor() {
+        System.out.println("I am Called open object constructor");
+    }
+}
